b9643b1c0cdc3bb2e4c3544070aa2262d9087862
hasnapulipra <hasnapulipra@gmail.com>
2022-07-26 11:17:20 +0530

    changes made

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q7b1*!e9t@ny1@4=u66q!i%@3)3c7a(#fqjg0a_es25it6lu+w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
